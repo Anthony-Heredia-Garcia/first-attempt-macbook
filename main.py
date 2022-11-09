5d5493155e29be79a317782b0000f999106e84b3
Anthony Garcia <tonydacoder@Anthonys-MBP.lan>
2022-11-09 08:44:10 -0500

    first commmit

---
+++ main.py
@@ -0,0 +1,11 @@
+def multiply(el):
+    return el * 2
+
+my_list = [1,2,3,4]
+
+new_list = list(map(multiply, my_list))
+better_list = list(map(lambda *el: el*3, [1,2,3,4,5]))
+
+print(new_list)
+
+print(better_list)
